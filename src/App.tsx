067d248b51cde522d1223bad4becd5922005f418
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-11 10:38:47 -0300

    feat: Add styled-components and define global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
