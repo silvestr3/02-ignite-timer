8b94af8559622beb194786b3eff8e32aba3c8e80
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-11 15:32:15 -0300

    feat: Create timer page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            placeholder="Dê um nome para seu projeto"
+            type="text"
+            list="task-suggestions"
+            id="task"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="Projeto 5" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            type="number"
+            id="minutesAmount"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} /> Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+};
