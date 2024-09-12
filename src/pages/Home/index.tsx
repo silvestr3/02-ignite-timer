a6f416239065252ccbaa275094474e34c8f12468
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-12 09:35:31 -0300

    Add hook-form and validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+
 import {
   CountdownContainer,
   FormContainer,
@@ -9,10 +13,33 @@ import {
   TaskInput,
 } from "./styles";
 
+const formSchema = z.object({
+  task: z.string().min(1, "Informe a tarefa"),
+  minutesAmount: z.number().min(5).max(60),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
 export const Home = () => {
+  const { register, handleSubmit, watch, reset } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const task = watch("task");
+
+  function handleCreateNewCycle(data: FormData) {
+    console.log(data);
+
+    reset();
+  }
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
@@ -20,6 +47,7 @@ export const Home = () => {
             type="text"
             list="task-suggestions"
             id="task"
+            {...register("task")}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -37,6 +65,7 @@ export const Home = () => {
             max={60}
             type="number"
             id="minutesAmount"
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -50,7 +79,7 @@ export const Home = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} /> Começar
         </StartCountdownButton>
       </form>
