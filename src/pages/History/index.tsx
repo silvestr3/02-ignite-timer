d47f787383c40ed32e089ab8872921369a3e171b
silvestr3 <fellipe.silvestre@outlook.com>
2024-09-13 11:27:52 -0300

    feat: Implement history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { HistoryContainer, HistoryList, StatusCell } from "./styles";
+import { CyclesContext } from "../../context/CyclesContext";
+import { formatDistanceToNow } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR";
 
 export const History = () => {
+  const { cycles } = useContext(CyclesContext);
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
@@ -17,46 +23,32 @@ export const History = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <StatusCell statusColor="red">Interrompido</StatusCell>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <StatusCell statusColor="green">Concluído</StatusCell>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <StatusCell statusColor="green">Concluído</StatusCell>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <StatusCell statusColor="yellow">Em andamento</StatusCell>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>
-                <StatusCell statusColor="green">Concluído</StatusCell>
-              </td>
-            </tr>
+            {cycles.map((cycle) => (
+              <tr key={cycle.id}>
+                <td>{cycle.task}</td>
+                <td>{cycle.minutesAmount} minutos</td>
+                <td>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    //@ts-expect-error
+                    locale: ptBr,
+                  })}
+                </td>
+                <td>
+                  {cycle.finishedDate && (
+                    <StatusCell statusColor="green">Concluído</StatusCell>
+                  )}
+
+                  {cycle.interruptedDate && (
+                    <StatusCell statusColor="red">Interrompido</StatusCell>
+                  )}
+
+                  {!cycle.interruptedDate && !cycle.finishedDate && (
+                    <StatusCell statusColor="yellow">Em andamento</StatusCell>
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </HistoryList>
